cf470ae14fdcc1998f69d5a4a6e790c8875a4967
goto-ui <onion753@gmail.com>
2020-03-30 17:05:10 +0000

    ログイン用のパラメーター設定とusernameのバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,24 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_accessor :login
+  validates :username,
+  :uniqueness => {
+    :case_sensitive => false
+  },
+  format: { with: /\A[a-z0-9]+\z/, message: "ユーザー名は半角英数字です"}
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable,
+         :authentication_keys => [:login]
+         
+         # 以下を追加
+  def self.find_first_by_auth_conditions(warden_conditions)
+      conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+        where(conditions).where(["username = :value OR lower(email) = lower(:value)", { :value => login }]).first
+    else
+        where(conditions).first
+    end
+  end
+
 end
